065fbb8d5d436147d88693c62006a0f8d644feb5
Andres Mareca <andres.mareca@gmail.com>
2018-05-04 15:36:07 +0200

    Load from env

--- src/throttle_app.erl
+++ src/throttle_app.erl
@@ -7,7 +7,16 @@
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for Coowry Core (cwc).
 start(_Type, _StartArgs) ->
-    throttle:start_link().
+    {ok, Pid} = throttle:start_link(),
+    case application:get_env(throttle, resources) of
+	{ok, Resources} ->
+	    lists:foreach(fun({Name, Init}) ->
+				  throttle:start_resource(Name, Init)
+			  end, Resources);
+	_ ->
+	    ok
+    end,
+    {ok, Pid}.
 
 %% @spec stop(_State) -> ServerRet
 %% @doc application stop callback for Coowry Core (cwc).
