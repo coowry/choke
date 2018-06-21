e62247f16ae595d3941c319de84dbf2a54d8113d
Andres Mareca <andres.mareca@gmail.com>
2018-06-21 11:30:41 +0200

    Change indent

--- src/throttle.erl
+++ src/throttle.erl
@@ -23,22 +23,22 @@
 %% @doc Start the supervisor.
 -spec start_link() -> supervisor:startlink_ret().
 start_link() ->
-    process_flag(trap_exit, true),
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+  process_flag(trap_exit, true),
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% @doc Create the context gen_server process, the function receive the Id
 %% of the context and the parameters to create throttle_counter. 
 -spec start_context(Id :: atom(),
-		     CounterInit :: {integer(), integer()}) -> {ok, pid()}.
+                    CounterInit :: {integer(), integer()}) -> {ok, pid()}.
 start_context(Id, {Limit, Timeout}) ->
-    ChildSpec = #{id => Id,
-		  start => {throttle_context, start_link, [Id, {Limit, Timeout}]},
-		  restart => permanent,
-		  shutdown => 10500,
-		  type => worker,
-		  modules => [throttle_context]
-     },
-    supervisor:start_child(?MODULE, ChildSpec).
+  ChildSpec = #{id => Id,
+                start => {throttle_context, start_link, [Id, {Limit, Timeout}]},
+                restart => permanent,
+                shutdown => 10500,
+                type => worker,
+                modules => [throttle_context]
+               },
+  supervisor:start_child(?MODULE, ChildSpec).
 
 
 %% @doc Update the counter of the CounterId belonging to the context 
@@ -47,71 +47,71 @@ start_context(Id, {Limit, Timeout}) ->
 %% Throw 'invalid_context' if the context doesn't exist.
 -spec check(atom(), any()) -> {ok, integer()} | {error, integer(), integer()}.
 check(ContextId, CounterId) ->
-    try
-	throttle_context:check(ContextId, CounterId)
-    catch
-	exit:_ -> throw(invalid_context)
-    end.
+  try
+    throttle_context:check(ContextId, CounterId)
+  catch
+    exit:_ -> throw(invalid_context)
+  end.
 
 %% @doc Get the counter of the CounterId belonging to the context 
 %% ContextId and return a pair {ok, count} if you are between the limit or 
 %% {error, timeout} if you exceed the limit.
 -spec peek(atom(), any()) -> {ok, integer()} | {error, integer(), integer()}.
 peek(ContextId, CounterId) ->
-    try
-	throttle_context:peek(ContextId, CounterId)
-    catch
-	exit:_ -> throw(invalid_context)
-    end.
+  try
+    throttle_context:peek(ContextId, CounterId)
+  catch
+    exit:_ -> throw(invalid_context)
+  end.
 
 
 %% @doc Restore the counter of the CounterId belonging to the context 
 %% ContextId and return a pair {ok, count}.
 -spec restore(atom(), any()) -> {ok, integer()}.
 restore(ContextId, CounterId) ->
-    try
-	throttle_context:restore(ContextId, CounterId)
-    catch
-	exit:_ -> throw(invalid_context)
-    end.
+  try
+    throttle_context:restore(ContextId, CounterId)
+  catch
+    exit:_ -> throw(invalid_context)
+  end.
 
 
 %% @doc Restart the context gen_server process independently of the state.
 -spec restart(atom()) -> ok.
 restart(ContextId) ->
-    try
-	case whereis(ContextId) of
-	    undefined -> throw(invalid_context);
-	    A -> 
-		unlink(A),
-		throttle_context:stop(ContextId)
-	end
-    catch
-	exit:_ -> throw(invalid_context)
-    end.
+  try
+    case whereis(ContextId) of
+      undefined -> throw(invalid_context);
+      A -> 
+        unlink(A),
+        throttle_context:stop(ContextId)
+    end
+  catch
+    exit:_ -> throw(invalid_context)
+  end.
 
 
 %% @doc Stop the context gen_server process independently of the state.
 -spec stop(atom() | pid()) -> ok.
 stop(ContextId) when is_atom(ContextId) ->
-    supervisor:terminate_child(?MODULE, ContextId),
-    case supervisor:delete_child(?MODULE, ContextId) of
-	{error, _} -> throw(invalid_context);
-	_ -> ok
-    end;
+  supervisor:terminate_child(?MODULE, ContextId),
+  case supervisor:delete_child(?MODULE, ContextId) of
+    {error, _} -> throw(invalid_context);
+    _ -> ok
+  end;
 
 stop(ContextId) when is_pid(ContextId) ->
-    case process_info(ContextId, registered_name) of
-	{registered_name, Name} ->
-	    stop(Name);
-	_ -> throw(invalid_context)
-    end.
+  case process_info(ContextId, registered_name) of
+    {registered_name, Name} ->
+      stop(Name);
+    _ -> throw(invalid_context)
+  end.
 
 
 %% @doc Stop the throttle supervisor process independently of the state.
 -spec stop() -> true.
 stop() ->
-    exit(whereis(?MODULE), shutdown).
+  exit(whereis(?MODULE), shutdown).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -119,8 +119,8 @@ stop() ->
 
 %% @doc Constructor of the throttle supervisor process.
 init([]) ->
-    MaxRestart = 6,
-    MaxTime = 3000,
-    {ok, {{one_for_one, MaxRestart, MaxTime}, []}}.
+  MaxRestart = 6,
+  MaxTime = 3000,
+  {ok, {{one_for_one, MaxRestart, MaxTime}, []}}.
 
 
--- src/throttle_app.erl
+++ src/throttle_app.erl
@@ -7,19 +7,19 @@
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for throttle.
 start(_Type, _StartArgs) ->
-    {ok, Pid} = throttle:start_link(),
-    case application:get_env(throttle, resources) of
-	{ok, Resources} ->
-	    lists:foreach(fun({Id, {Limit, Timeout}}) ->
-				  throttle:start_context(Id, {Limit, Timeout})
-			  end, Resources);
-	_ ->
-	    ok
-    end,
-    {ok, Pid}.
+  {ok, Pid} = throttle:start_link(),
+  case application:get_env(throttle, resources) of
+    {ok, Resources} ->
+      lists:foreach(fun({Id, {Limit, Timeout}}) ->
+                        throttle:start_context(Id, {Limit, Timeout})
+                    end, Resources);
+    _ ->
+      ok
+  end,
+  {ok, Pid}.
 
 %% @spec stop(_State) -> ServerRet
 %% @doc application stop callback for throttle.
 stop(_State) ->
-    throttle:stop(),
-    ok.
+  throttle:stop(),
+  ok.
